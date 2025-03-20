728924336f7a44418412046cb4c38e00e20e42d6
Rishabh2457 <rishabh.kushwah_cs.aiml21@gla.ac.in>
2025-03-21 00:27:12 +0530

    [Rishabh] Add : Adding user controller uC10

--- MiddleWare/obj/Debug/net6.0/MiddleWare.AssemblyInfo.cs
+++ MiddleWare/obj/Debug/net6.0/MiddleWare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiddleWare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788ccd1af5873a6b458a25d3ee9348876c06eaf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+576d06af573cc6a119a78437c7235c808eaf1bdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiddleWare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiddleWare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
